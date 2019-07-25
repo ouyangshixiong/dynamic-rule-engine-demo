af401d33e2a5856af2417a35f4882469b5c5682f
ouyangshixiong <ouyangshixiong@gmail.com>
2019-07-25 11:00:32 +0800

    集成yaml文件读取测试用例

--- src/test/java/com/example/ruleengine/DynamicRuleEngineDemoApplicationTests.java
+++ src/test/java/com/example/ruleengine/DynamicRuleEngineDemoApplicationTests.java
@@ -2,13 +2,22 @@ package com.example.ruleengine;
 
 import com.example.ruleengine.rules.*;
 import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rule;
 import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.core.DefaultRulesEngine;
+import org.jeasy.rules.mvel.MVELRuleFactory;
+import org.jeasy.rules.support.YamlRuleDefinitionReader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mvel2.MVEL;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -136,6 +145,25 @@ public class DynamicRuleEngineDemoApplicationTests {
         facts.put("age",age);
         facts.put("gender",gender);
         sb.append("activationRuleGroupTest--------- fire rule engine with age=" + age + " gender=" + gender +"\n");
+        rulesEngine.fire(rules,facts);
+        System.out.println(sb.toString());
+    }
+
+    @Test
+    public void salaryTest() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        Facts facts = new Facts();
+        int age = 30;
+        String gender = "male";
+        facts.put("age",age);
+        facts.put("gender",gender);
+        facts.put("salary",900);
+
+        MVELRuleFactory ruleFactory = new MVELRuleFactory(new YamlRuleDefinitionReader());
+        Rule salaryRule = ruleFactory.createRule(new FileReader(new ClassPathResource("salary-rule.yml").getFile()));
+        Rules rules = new Rules();
+        rules.register(new EnrollUnitRuleGroup(new AgeRule(sb), new GenderRule(sb), salaryRule));
+        RulesEngine rulesEngine = new DefaultRulesEngine();
         rulesEngine.fire(rules,facts);
         System.out.println(sb.toString());
     }
